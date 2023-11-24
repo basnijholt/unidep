65eb6b1203ae10392b1c6557ca593b62ddff3750
Bas Nijholt <bas@nijho.lt>
2023-11-24 14:42:34 -0800

    Add backticks around file names

--- unidep.py
+++ unidep.py
@@ -746,7 +746,7 @@ def main() -> None:  # pragma: no cover
     output_file = None if args.stdout else args.output
     write_conda_environment_file(env_spec, output_file, args.name, verbose=verbose)
     if output_file:
-        found_files_str = ", ".join(str(f) for f in found_files)
+        found_files_str = ", ".join(f"`{f}`" for f in found_files)
         print(f"✅ Generated environment file at `{output_file}` from {found_files_str}")
 
 
