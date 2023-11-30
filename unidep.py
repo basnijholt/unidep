2a1951707215c2d2b1ff24f14f9e09414de34583
Bas Nijholt <bas@nijho.lt>
2023-11-30 13:23:58 -0800

    Check for `$CONDA_PREFIX` and make sure the right environment is used (#35)
    * Check for $CONDA_PREFIX and make sure the right environment is used
    
    * Change error message
    
    * Skip in coverage

--- unidep.py
+++ unidep.py
@@ -1546,6 +1546,24 @@ def _mismatch_report(
     warnings.warn(full_error_message, stacklevel=2)
 
 
+def _check_conda_prefix() -> None:  # pragma: no cover
+    """Check if sys.executable is in the $CONDA_PREFIX."""
+    if "CONDA_PREFIX" not in os.environ:
+        return
+    conda_prefix = os.environ["CONDA_PREFIX"]
+    if sys.executable.startswith(str(conda_prefix)):
+        return
+    msg = (
+        "UniDep should be run from the current Conda environment for correct"
+        " operation. However, it's currently running with the Python interpreter"
+        f" at `{sys.executable}`, which is not in the active Conda environment"
+        f" (`{conda_prefix}`). Please install and run UniDep in the current"
+        " Conda environment to avoid any issues."
+    )
+    warnings.warn(msg, stacklevel=2)
+    sys.exit(1)
+
+
 def main() -> None:
     """Main entry point for the command-line tool."""
     args = _parse_args()
@@ -1586,6 +1604,7 @@ def main() -> None:
         )
         print(_escape_unicode(args.separator).join(env_spec.conda))  # type: ignore[arg-type]
     elif args.command == "install":
+        _check_conda_prefix()
         _install_command(
             conda_executable=args.conda_executable,
             dry_run=args.dry_run,
