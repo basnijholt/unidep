f3f59439bdff9d817de8df5a3fdc00f4d5a828b6
Bas Nijholt <bas@nijho.lt>
2023-12-06 19:34:54 -0800

    Test doubly nested folder

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,6 +1,7 @@
 """unidep tests."""
 from __future__ import annotations
 
+import shutil
 import subprocess
 from pathlib import Path
 
@@ -100,4 +101,57 @@ def test_unidep_install_all_dry_run() -> None:
     assert (
         f"-m pip install -e {REPO_ROOT}/example/project1 -e {REPO_ROOT}/example/project2 -e {REPO_ROOT}/example/project3`"
         in result.stdout
+    )
+
+
+def test_doubly_nested_project_folder_installable(
+    tmp_path: Path,
+) -> None:
+    example_folder = tmp_path / "example"
+    shutil.copytree(REPO_ROOT / "example", example_folder)
+
+    # Add an extra project
+    extra_projects = example_folder / "extra_projects"
+    extra_projects.mkdir(exist_ok=True, parents=True)
+    project4 = extra_projects / "project4"
+    project4.mkdir(exist_ok=True, parents=True)
+    (project4 / "requirements.yaml").write_text("includes: [../../project1]")
+    pyproject_toml = "\n".join(  # noqa: FLY002
+        (
+            "[build-system]",
+            'requires = ["setuptools", "unidep"]',
+            'build-backend = "setuptools.build_meta"',
+        ),
+    )
+
+    (project4 / "pyproject.toml").write_text(pyproject_toml)
+    setup = "\n".join(  # noqa: FLY002
+        (
+            "from setuptools import setup",
+            'setup(name="project4", version="0.1.0", description="yolo", py_modules=["project1"])',
+        ),
+    )
+    (project4 / "setup.py").write_text(setup)
+    (project4 / "project4.py").write_text("print('hello')")
+
+    # Run the unidep install command
+    result = subprocess.run(
+        [  # noqa: S607, S603
+            "unidep",
+            "install",
+            "--dry-run",
+            "--editable",
+            str(project4 / "requirements.yaml"),
+        ],
+        check=True,
+        capture_output=True,
+        text=True,
+    )
+
+    assert (
+        f"-m pip install -e {tmp_path}/example/project1 -e {tmp_path}/example/project2 -e {tmp_path}/example/project3`"
+        in result.stdout
+    )
+    assert (
+        f"pip install -e {tmp_path}/example/extra_projects/project4`" in result.stdout
     )
