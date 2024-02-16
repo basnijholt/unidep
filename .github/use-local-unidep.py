25eb7aa20a955cc086168e37fce831d66be399b6
Bas Nijholt <bas@nijho.lt>
2024-02-16 10:04:57 -0800

    Print

--- .github/use-local-unidep.py
+++ .github/use-local-unidep.py
@@ -1,8 +1,13 @@
+"""Update `pyproject.toml` in each example project to use local `unidep`."""
 from pathlib import Path
 
 REPO_ROOT = Path(__file__).resolve().parent.parent
 EXAMPLE_DIR = REPO_ROOT / "examples"
 PROJECT_DIRS = [p for p in EXAMPLE_DIR.iterdir() if p.name.endswith("_project")]
+
+print(
+    f"REPO_ROOT: {REPO_ROOT}, EXAMPLE_DIR: {EXAMPLE_DIR}, PROJECT_DIRS: {PROJECT_DIRS}",
+)
 
 for project_dir in PROJECT_DIRS:
     # find the line with `requires = [` in `pyproject.toml` in each project
