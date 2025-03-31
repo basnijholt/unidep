c9d9c1b8bd315facf38140a911804828b67b3f4d
Bas Nijholt <bas@nijho.lt>
2025-03-31 11:14:23 -0700

    make tomli a dependency for <3.11

--- .github/use-local-unidep.py
+++ .github/use-local-unidep.py
@@ -12,20 +12,14 @@ print(
 
 for project_dir in PROJECT_DIRS:
     # find the line with `requires = [` in `pyproject.toml` in each project
-    # directory and replace `"unidep"` or `"unidep[toml]"` with
-    # `"unidep @ file://<abs-path-to-repo-root>"`` or
-    # `"unidep[toml] @ file://<abs-path-to-repo-root>"` respectively
+    # directory and replace `"unidep"` with
+    # `"unidep @ file://<abs-path-to-repo-root>"``
     pyproject_toml = project_dir / "pyproject.toml"
     lines = pyproject_toml.read_text().splitlines()
     repo_root = REPO_ROOT.as_posix()  # convert to posix path for windows
     for i, line in enumerate(lines):
         if "requires = [" in line:
-            if "unidep[toml]" in line:
-                lines[i] = line.replace(
-                    "unidep[toml]",
-                    f"unidep[toml] @ file://{repo_root}",
-                )
-            elif "unidep" in line:
+            if "unidep" in line:
                 lines[i] = line.replace("unidep", f"unidep @ file://{repo_root}")
             break
     pyproject_toml.write_text("\n".join(lines))
